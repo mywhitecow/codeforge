d43fc05d8bbc7db69d3ee9a6d19cd8a30b3415c2
Jhosep-M <molloemerson10@gmail.com>
2026-04-18 21:07:00 -0400

    error critico

--- frontend/web-lumina/src/main.server.ts
+++ frontend/web-lumina/src/main.server.ts
@@ -1,9 +1,8 @@
 import 'zone.js'
-import { BootstrapContext, bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/app';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
 const bootstrap = (context: BootstrapContext) =>
-    bootstrapApplication(App, config, context);
-
+    bootstrapApplication(AppComponent, config, context);
 export default bootstrap;
