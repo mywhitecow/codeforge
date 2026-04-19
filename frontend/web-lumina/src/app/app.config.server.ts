369002968df4eab3098494af1184ce2eab7600ab
Jhosep-M <molloemerson10@gmail.com>
2026-04-18 23:28:54 -0400

    error:ng build --ssr

--- frontend/web-lumina/src/app/app.config.server.ts
+++ frontend/web-lumina/src/app/app.config.server.ts
@@ -1,15 +1,11 @@
 // app.config.server.ts
 
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering, provideServerRoutesConfig } from '@angular/ssr';
+import { provideServerRendering } from '@angular/ssr';
 import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering(),
-    provideServerRoutesConfig(serverRoutes),
-  ],
+  providers: [provideServerRendering()],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
