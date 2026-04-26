b83f2c6e8560ce95bd564e4f31fb3111197db934
Jhosep-M <molloemerson10@gmail.com>
2026-04-25 22:53:17 -0400

    api_test

--- frontend/web-lumina/src/environments/environment.prod.ts
+++ frontend/web-lumina/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tudominio.com/api'
+  apiUrl: 'http://localhost:8000/api',
+ 
 };
--- frontend/web-lumina/src/environments/environment.ts
+++ frontend/web-lumina/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api' // URL de tu backend PHP
+  apiUrl: 'http://localhost:8000/api',
+ 
+   // URL de tu backend PHP
 };
